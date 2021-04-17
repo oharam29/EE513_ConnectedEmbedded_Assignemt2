fabfa9c30dd866f6493361cf025f7e200098c34c
oharam29 <56358284+oharam29@users.noreply.github.com>
2021-04-17 15:37:05 +0100

    added json parsing to sub1

--- EE513_Assignment2/src/FirstSubscriber.cpp
+++ EE513_Assignment2/src/FirstSubscriber.cpp
@@ -5,19 +5,74 @@
  *      Author: Nova6
  */
 
-#include "FirstSubscriber.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "MQTTClient.h"
+#include<json-c/json.h>
 
-First_Subscriber::First_Subscriber() {
-	// TODO Auto-generated constructor stub
+#define ADDRESS     "tcp://192.168.1.15:1883"
+#define CLIENTID    "Subscriber1"
+#define AUTHMETHOD  "mike"
+#define AUTHTOKEN   "password"
+#define TOPIC       "ee513/CPUTemp"
+#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
 
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+    printf("Subscriber Number 1 has received Message with token value %d\n", dt);
+    deliveredtoken = dt;
 }
 
-First_Subscriber::~First_Subscriber() {
-	// TODO Auto-generated destructor stub
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+
+	struct json_object *parsed;
+	struct json_object *time;
+
+	parsed = json_tokener_parse((char*)message->payload);
+	json_object_object_get_ex(parsed_json, "CurrentTime", &time);
+    printf("Current Time: %s    (topic: %s)\n", json_object_get_string(time), topicName);
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
 }
 
-First_Subscriber::First_Subscriber(const First_Subscriber &other) {
-	// TODO Auto-generated constructor stub
-
+void connlost(void *context, char *cause) {
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
 }
+
+int main(int argc, char* argv[]) {
+    MQTTClient client;
+    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+    int rc;
+    int ch;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    opts.keepAliveInterval = 20;
+    opts.cleansession = 1;
+    opts.username = AUTHMETHOD;
+    opts.password = AUTHTOKEN;
+
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+    if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
+    MQTTClient_subscribe(client, TOPIC, QOS);
+
+    do {
+        ch = getchar();
+    } while(ch!='Q' && ch != 'q');
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}
+
 
--- EE513_Assignment2/src/FirstSubscriber.h
+++ EE513_Assignment2/src/FirstSubscriber.h
@@ -5,82 +5,19 @@
  *      Author: Nova6
  */
 
-/*
- * subscribertemplate.cpp
- *
- *  Created on: 15 Apr 2021
- *      Author: Nova6
- */
+#ifndef FIRSTSUBSCRIBER_H_
+#define FIRSTSUBSCRIBER_H_
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "MQTTClient.h"
+class FirstSubsciber {
+public:
+	subscriber_template();
+	virtual ~subscriber_template();
+	subscriber_template(const subscriber_template &other);
 
-#define ADDRESS     "tcp://192.168.1.15:1883"
-#define CLIENTID    "Subscriber1"
-#define AUTHMETHOD  "mike"
-#define AUTHTOKEN   "password"
-#define TOPIC       "ee513/CPUTemp"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
+	void connlost(void *context, char *cause);
+	int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+	void delivered(void *context, MQTTClient_deliveryToken dt);
+};
 
-volatile MQTTClient_deliveryToken deliveredtoken;
-
-void delivered(void *context, MQTTClient_deliveryToken dt) {
-    printf("Message with token value %d delivery confirmed\n", dt);
-    deliveredtoken = dt;
-}
-
-int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
-    int i;
-    char* payloadptr;
-    printf("Message arrived\n");
-    printf("     topic: %s\n", topicName);
-    printf("   message: ");
-    payloadptr = (char*) message->payload;
-    for(i=0; i<message->payloadlen; i++) {
-        putchar(*payloadptr++);
-    }
-    putchar('\n');
-    MQTTClient_freeMessage(&message);
-    MQTTClient_free(topicName);
-    return 1;
-}
-
-void connlost(void *context, char *cause) {
-    printf("\nConnection lost\n");
-    printf("     cause: %s\n", cause);
-}
-
-int main(int argc, char* argv[]) {
-    MQTTClient client;
-    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
-    int rc;
-    int ch;
-
-    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    opts.keepAliveInterval = 20;
-    opts.cleansession = 1;
-    opts.username = AUTHMETHOD;
-    opts.password = AUTHTOKEN;
-
-    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
-    if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
-    }
-    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
-           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
-    MQTTClient_subscribe(client, TOPIC, QOS);
-
-    do {
-        ch = getchar();
-    } while(ch!='Q' && ch != 'q');
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-    return rc;
-}
-
+#endif /* FIRSTSUBSCRIBER_H_ */
 
